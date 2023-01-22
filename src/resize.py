cb4b629a87c6113442e69782a57a1ac1fe347786
Thomas MICHEL <thomas.michel.thmi@gmail.com>
2023-01-22 22:34:24 +0100

    add script to resize all the files from a folder

---
+++ src/resize.py
@@ -0,0 +1,14 @@
+import cv2
+import os
+
+img_dir = "denoisedflickrSR/"
+img_list = os.listdir(img_dir)
+
+for img_path in img_list:
+    print(img_path)
+    img = cv2.imread(img_dir+img_path,cv2.IMREAD_UNCHANGED)
+    width = int(img.shape[1] /3.5)
+    height = int(img.shape[0] /3.5)
+    dim = (width, height)
+    img = cv2.resize(img, dim, interpolation=cv2.INTER_CUBIC)
+    cv2.imwrite(img_dir+img_path[:-4]+"DS.png", img)
